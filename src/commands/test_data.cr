9c25b21522b4d8285cd374d62524f82b83983b0e
Tatsiujin Chin <c910335@gmail.com>
2019-10-30 16:27:43 +0800

    add reducer to test data

--- src/commands/test_data.cr
+++ src/commands/test_data.cr
@@ -3,14 +3,17 @@ class NCTU::OJ::TestData < Admiral::Command
   define_help description: "Upload test data"
   define_flag problem_id : Int32, description: "Problem id", short: "p"
   define_flag dir : String, description: "Test data dir", short: "d"
-  define_flag number : Int32, description: "The number of test data", default: 10, short: "n"
-  define_flag sample : Int32, description: "The count of sample test data", default: 2, short: "s"
+  define_flag number : Int32, description: "The number of test data includes sample", default: 10, short: "n"
+  define_flag sample : Int32, description: "The number of sample test data", default: 2, short: "s"
   define_flag clear : Bool, description: "Delete test data before upload", default: false, short: "c"
   define_flag time : Int32, description: "Time limit (ms)", default: 1000, short: "t"
   define_flag memory : Int32, description: "Memory limit (KB)", default: 262144, short: "m"
   define_flag output : Int32, description: "Output limit (KB)", default: 262144, short: "o"
+  define_flag reducer : String, description: "Reducer for score (must be min or sum)", default: "min", short: "r"
 
   def run
+    raise "Unknown reducer." unless ["min", "sum"].includes? flags.reducer
+
     load_config
 
     problem_id = if p = flags.problem_id
@@ -27,7 +30,7 @@ class NCTU::OJ::TestData < Admiral::Command
             gets.to_s
           end
 
-    number, sample, clear, time, memory, output = flags.number, flags.sample, flags.clear, flags.time, flags.memory, flags.output
+    number, sample, clear, time, memory, output, reducer = flags.number, flags.sample, flags.clear, flags.time, flags.memory, flags.output, flags.reducer
 
     if clear
       puts "Clearing..."
@@ -36,7 +39,21 @@ class NCTU::OJ::TestData < Admiral::Command
 
     puts "Uploading..."
 
+    score, border = if reducer == "min"
+                      {100, 0}
+                    else
+                      {100 // (number - sample), number - 100 % (number - sample) + 1}
+                    end
+
     (1..number).each do |i|
+      s = if reducer == "min"
+            100
+          elsif i <= sample
+            0
+          else
+            score += 1 if i == border
+            score
+          end
       client.upload_test_data(
         problem_id,
         i <= sample,
@@ -45,7 +62,7 @@ class NCTU::OJ::TestData < Admiral::Command
         time,
         memory,
         output,
-        100
+        s
       )
       print "#{i}.."
     end
