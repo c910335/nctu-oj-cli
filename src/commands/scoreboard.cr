2fe549c36755fe04e702942e29a83284adb78fa7
Tatsiujin Chin <c910335@gmail.com>
2019-11-13 16:23:15 +0800

    add judge type to scoreboard

--- src/commands/scoreboard.cr
+++ src/commands/scoreboard.cr
@@ -2,28 +2,37 @@ class NCTU::OJ::Scoreboard < Admiral::Command
   include CommandHelper
   define_help description: "Generate a scoreboard into scoreboard.html"
   define_flag output_filename : String, description: "Output filename", short: "o", default: "scoreboard.html"
+  define_flag judge : String, description: "Judge type (must be pass or score)", short: "j", default: "pass"
 
   def run
+    raise "Unknown judge type." unless {"pass", "score"}.includes? flags.judge
+
     load_config
     pull_users
     pull_submissions
 
     puts "Processing..."
 
-    judges = Array(Array(Bool?)).new(users.size) do
-      Array(Bool?).new(problems.size) { nil }
+    judges = Array(Array(Int32?)).new(users.size) do
+      Array(Int32?).new(problems.size) { nil }
     end
     multi_submissions.each_with_index do |ss, i|
       ss.each do |s|
-        judges[user_id_to_i[s.user_id]][i] ||= s.score >= 100 && !invalids.includes?({user_id_to_name[s.user_id], s.problem_id})
+        if invalids.includes?({user_id_to_name[s.user_id], s.problem_id})
+          judges[user_id_to_i[s.user_id]][i] = 0
+        else
+          j = judges[user_id_to_i[s.user_id]][i]
+          judges[user_id_to_i[s.user_id]][i] = s.score if j.nil? || s.score > j
+        end
       end
     end
 
-    student_totals = judges.map { |js| js.map { |j| j ? 1 : 0 }.sum }                              # ameba:disable Lint/UselessAssign
-    problem_totals = Array(Int32).new(problems.size) { |i| judges.map { |js| js[i] ? 1 : 0 }.sum } # ameba:disable Lint/UselessAssign
+    student_totals = judges.map { |js| js.map { |j| j.try &.>=(100) ? 1 : 0 }.sum }                              # ameba:disable Lint/UselessAssign
+    problem_totals = Array(Int32).new(problems.size) { |i| judges.map { |js| js[i].try &.>=(100) ? 1 : 0 }.sum } # ameba:disable Lint/UselessAssign
 
     puts "Outputing..."
 
+    judge_type = flags.judge # ameba:disable Lint/UselessAssign
     File.write(flags.output_filename, Kilt.render("src/views/scoreboard.slang"))
   end
 end
